44699fd0b555ce114c5fe299a76072abfbf785c2
Leszek Macura <leszek@macura.co>
2018-12-09 18:56:20 +0100

    Zadanie 20_04 Gotowe

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-import { getCountries } from '../src/actions/actions-countries';
-
-
+import routes from './routes';
 
 render(
-
     <Provider store={store}>
-        <div>
-            <DevTools />
-            <h1>Hi there ;)</h1>
-        </div>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
-
-
 );
-store.dispatch(getCountries());
